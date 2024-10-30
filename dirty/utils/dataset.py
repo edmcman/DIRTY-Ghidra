539eb6df0ef8fa7ac74a49b0fe0a495037c73335
Edward J. Schwartz <edmcman@cmu.edu>
2024-10-30 10:33:33 -0400

    wds API change

--- dirty/utils/dataset.py
+++ dirty/utils/dataset.py
@@ -200,7 +200,7 @@ def identity(x):
 def get_src_len(e):
     return e.source_seq_length
 
-class Dataset(wds.Dataset):
+class Dataset(wds.WebDataset):
 
     SHUFFLE_BUFFER = 5000
     SORT_BUFFER = 512
@@ -239,7 +239,7 @@ class Dataset(wds.Dataset):
         # XXX: Limit number of files we read?  Right
         # now we use all of them.
         try:
-            line_dataset = wds.Dataset(urls).pipe(Dataset._file_iter_to_line_iter)
+            line_dataset = wds.WebDataset(urls).pipe(Dataset._file_iter_to_line_iter)
             print(f"URLs: {urls} dataset: {line_dataset}")
             self.len = sum(1 for _line in line_dataset)
         except:
